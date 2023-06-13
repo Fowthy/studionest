c50972abf89eef7ff4a5c526b9b591d36ae10263
Alex Svetoslavov <xfowth@gmail.com>
2023-06-13 23:15:14 +0200

    fixed client build errorQ

--- client/src/pages/api/admin/rooms/createroom.ts
+++ client/src/pages/api/admin/rooms/createroom.ts
@@ -1,18 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import FormData from 'form-data'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
       const { name, location, description, pricePerHour, type, owner, owner_uid } = req.body;
       console.log(req.body, ' tuk')
-      const formData = new FormData();
-      formData.append('room', JSON.stringify({ name, location, description, pricePerHour, type, owner, owner_uid }));
-
+      let formdata = {
+        room: req.body
+      }
       const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'multipart/form-data' },
-        body: req.body,
+        body: formdata.room,
       };
 
       const response = await fetch(`${process.env.SERVER_HOST}/api/roomman/room`, requestOptions); // Replace with your FastAPI server URL
