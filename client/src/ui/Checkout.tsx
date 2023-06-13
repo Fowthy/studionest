7b06d78be770ef0e936bf6b2744683400daa14d7
Alex Svetoslavov <xfowth@gmail.com>
2023-06-13 23:56:22 +0200

    temporary removed stripe lib

--- client/src/ui/Checkout.tsx
+++ client/src/ui/Checkout.tsx
@@ -1,37 +1,37 @@
-import { loadStripe } from '@stripe/stripe-js';
-import axios from 'axios';
+// import { loadStripe } from '@stripe/stripe-js';
+// import axios from 'axios';
 
 
 
-const stripePromise = loadStripe(
-    "pk_test_51M9GZ7DYaugzDeTANNW4s8xTRgOLm3fEHIOI3EDZUbxJbEjYhes6wPKN4sGa61TZRcg2IuqvMrhjDE5YFRHf2CXb009alThMnG"
-  );
+// const stripePromise = loadStripe(
+//     "pk_test_51M9GZ7DYaugzDeTANNW4s8xTRgOLm3fEHIOI3EDZUbxJbEjYhes6wPKN4sGa61TZRcg2IuqvMrhjDE5YFRHf2CXb009alThMnG"
+//   );
 
 export default function Checkout({cart}: any) {
     const handleCheckout = async () => {
       try {
-        const stripe = await stripePromise;
+        // const stripe = await stripePromise;
 
-        let ss = {
-            "item1": "daaaaa, da",
-            "item2": "daaaaa",
-            "item3": "daaa",
-            "item4": "daaaada",
-        }
+        // let ss = {
+        //     "item1": "daaaaa, da",
+        //     "item2": "daaaaa",
+        //     "item3": "daaa",
+        //     "item4": "daaaada",
+        // }
 
-        const checkoutSession = await axios.post("/api/checkout-session", {
-          ss,
-        });
+        // const checkoutSession = await axios.post("/api/checkout-session", {
+        //   ss,
+        // });
 
 
-        if(stripe) {
-            const result = await stripe.redirectToCheckout({
-              sessionId: checkoutSession.data.id,
-            });
-            if (result.error) {
-              alert(result.error.message);
-            }
-        }
+        // if(stripe) {
+        //     const result = await stripe.redirectToCheckout({
+        //       sessionId: checkoutSession.data.id,
+        //     });
+        //     if (result.error) {
+        //       alert(result.error.message);
+        //     }
+        // }
   
       } catch (error) {
         console.log(error);
